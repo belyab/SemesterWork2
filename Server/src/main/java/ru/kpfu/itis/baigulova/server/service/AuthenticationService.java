0e098adf4d748ec0ab00fa0e9d5694186a4134b4
Elmira <bbelyab@gmail.com>
2021-12-04 19:36:38 +0300

    add quiz.csv

--- Server/src/main/java/ru/kpfu/itis/baigulova/server/service/AuthenticationService.java
+++ Server/src/main/java/ru/kpfu/itis/baigulova/server/service/AuthenticationService.java
@@ -7,8 +7,8 @@ import java.util.List;
 
 public class AuthenticationService {
     private List<Client> clients = new ArrayList<Client>(){
-        {add(new Client("test", "123"));}
-        {add(new Client("test2", "123"));}
+        {add(new Client("test1", "pass1"));}
+        {add(new Client("test2", "pass2"));}
     };
 
     public Client authenticate(String name, String pass){
